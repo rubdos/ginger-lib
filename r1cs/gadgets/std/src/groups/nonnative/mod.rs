8da0f93a20649f4b2e3daaa107c7456d725b0ec1
DanieleDiBenedetto <daniele@horizenlabs.io>
2021-09-14 04:15:36 -0400

    Minor changes

--- r1cs/gadgets/std/src/groups/nonnative/mod.rs
+++ r1cs/gadgets/std/src/groups/nonnative/mod.rs
@@ -1,3 +1,5 @@
 //! A module for simulating the group arithmetics of short Weierstrass curves 
 //! over non-native fields. 
-pub mod nonnative_group_gadget; 
+pub mod nonnative_group_gadget;
+
+// TODO: Adopt a similar structure as for the module ../group/curves
