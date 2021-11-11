c3dad08b3c5e106c2ba88fff5883eedb28f3f6f8
DanieleDiBenedetto <daniele@horizenlabs.io>
2021-11-11 09:58:38 +0100

    Added sqrt field test for ed25519 fields

--- algebra/src/fields/ed25519/tests.rs
+++ algebra/src/fields/ed25519/tests.rs
@@ -1,7 +1,4 @@
-use crate::fields::{
-    ed25519::{fq::Fq, fr::Fr},
-    tests::{field_test, primefield_test},
-};
+use crate::fields::{ed25519::{fq::Fq, fr::Fr}, tests::{field_test, primefield_test, sqrt_field_test}};
 
 #[test]
 fn test_ed25519_fr() {
@@ -9,6 +6,7 @@ fn test_ed25519_fr() {
     let b: Fr = rand::random();
     field_test(a, b);
     primefield_test::<Fr>();
+    sqrt_field_test(b);
 }
 
 #[test]
@@ -17,6 +15,7 @@ fn test_ed25519_fq() {
     let b: Fq = rand::random();
     field_test(a, b);
     primefield_test::<Fq>();
+    sqrt_field_test(b);
 }
 
 //TODO: Add tests with hardcoded data
